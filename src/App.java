5b6c4e7c2fdbab8b75ef3c78c70b698db3dde25e
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2023-02-11 17:36:14 +0530

    Datatypes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       public static void main(String[] args) throws Exception {
+        //Datatypes
+        final int num1 = 2;
+        String s = "Hello";
+        float num2 = 20.00f;
+        double num3 = 20;
+        boolean flag = true;
+        char c = 'a';
+        System.out.println("int:-" + num1);
+        System.out.println("String:-" + s);
+        System.out.println("Float:-" + num2);
+        System.out.println("double:-" + num3);
+        System.out.println("Boolean:-" + flag);
+        System.out.println("char:-" + c);
+        //Literals
+        // for binary
+        int binary = 0b101;
+        System.out.print("Binary:-" + binary);
+        
+
+
+
     }
 }
